b740ba884cd4601b48861267920dbfcf6dedba44
heganghudanni <heganghudanni@sina.com>
2019-11-07 15:48:12 +0800

    切小鸡鸡

--- src/com/atguigu/git/dugujiujian.java
+++ src/com/atguigu/git/dugujiujian.java
@@ -4,7 +4,7 @@ public class dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("¶À¹Â¾Å½£");
-		System.out.println("yyyyyyy");
+		System.out.println("yyyyyyyyyyyyyy");
 	}
 
 }
